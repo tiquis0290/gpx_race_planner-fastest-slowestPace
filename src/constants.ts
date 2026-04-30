9ec9bb716560f7fcbdf0a1416fbe10291be044d4
Marek Maslowski <marek1.maslowski@gmail.com>
2026-04-30 14:55:45 +0200

    chang git url and label for original author

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 import type { Lang } from './i18n/translations';
 
 // App / author links shown in the footer
-export const GITHUB_URL   = 'https://github.com/martinkobelka/gpx_race_planner';
+export const GITHUB_URL   = 'https://github.com/tiquis0290/gpx_race_planner-improvements';
 export const AUTHOR_URL   = 'https://martinkobelka.cz';
-export const AUTHOR_LABEL = 'martinkobelka.cz';
+export const AUTHOR_LABEL = 'original author: martinkobelka.cz';
 
 // Available UI languages
 export const LANGS: { value: Lang; label: string }[] = [
